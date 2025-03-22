fc092453023acb91b79137db8c6b46d1312b1ffe
Morten Brenna <rmortenb@gmail.com>
2025-03-22 16:03:15 +0100

    Added type for 2d coords

--- include/maggie_vec.h
+++ include/maggie_vec.h
@@ -3,6 +3,13 @@
 
 #include <math.h>
 
+typedef struct vec2
+{
+	float x, y;
+} vec2;
+
+/*****************************************************************************/
+
 typedef struct vec3
 {
 	float x, y, z;
